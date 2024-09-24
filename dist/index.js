e7917621ed34ae935048de7fe1997b42102eec1b
Abdalrahman Aldabbas <aldabbas333abdalrahman@gmail.com>
2024-09-24 06:03:37 +0300

    compress images

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,32 @@
 "use strict";
 let next = document.getElementById("next");
 let prev = document.getElementById("prev");
-next === null || next === void 0 ? void 0 : next.addEventListener("click", () => {
+let slide = false;
+function nextEffect() {
     var _a;
     const images = document.querySelectorAll(".item");
-    console.log("clicked");
     (_a = document.querySelector(".slider")) === null || _a === void 0 ? void 0 : _a.appendChild(images[0]);
-});
-prev === null || prev === void 0 ? void 0 : prev.addEventListener("click", () => {
+}
+function prevEffect() {
     var _a;
     const images = document.querySelectorAll(".item");
     (_a = document.querySelector(".slider")) === null || _a === void 0 ? void 0 : _a.prepend(images[images.length - 1]);
+}
+next === null || next === void 0 ? void 0 : next.addEventListener("click", () => {
+    nextEffect();
+    slide = false;
+    let timeOut = setTimeout(() => {
+        slide = true;
+    }, 10000);
 });
+prev === null || prev === void 0 ? void 0 : prev.addEventListener("click", () => {
+    prevEffect();
+    slide = false;
+    let timeOut = setTimeout(() => {
+        slide = true;
+    }, 10000);
+});
+let clicker = setInterval(() => {
+    if (slide)
+        nextEffect();
+}, 5000);
--- js/index.ts
+++ js/index.ts
@@ -1,14 +1,40 @@
 let next = document.getElementById("next");
 let prev = document.getElementById("prev");
 
-next?.addEventListener("click", () => {
+let slide = false;
+
+function nextEffect() {
   const images = document.querySelectorAll(".item");
 
-  console.log("clicked");
   document.querySelector(".slider")?.appendChild(images[0]);
-});
-prev?.addEventListener("click", () => {
+}
+
+function prevEffect() {
   const images = document.querySelectorAll(".item");
 
   document.querySelector(".slider")?.prepend(images[images.length - 1]);
+}
+
+next?.addEventListener("click", () => {
+  nextEffect();
+
+  slide = false;
+
+  let timeOut = setTimeout(() => {
+    slide = true;
+  }, 10000);
 });
+
+prev?.addEventListener("click", () => {
+  prevEffect();
+
+  slide = false;
+
+  let timeOut = setTimeout(() => {
+    slide = true;
+  }, 10000);
+});
+
+let clicker = setInterval(() => {
+  if (slide) nextEffect();
+}, 5000);
